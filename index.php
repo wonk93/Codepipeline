630912070e1d67165a1ce9d00d5f86a6ebc56c34
Daniel Salas Rodríguez <123897061+wonk93@users.noreply.github.com>
2025-05-06 23:39:41 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Tajamar Media</title>
+</head>
+<body>
+<h1 align="center">Buenos días, estudiantes </h1>
+<br>
+<div align="center">
+<img src="playa.jpg">
+</div>
+</body>
+</html>

c1cd7f9588b3291e2ed15f5f737bbf111551c2ac
Daniel Salas Rodríguez <123897061+wonk93@users.noreply.github.com>
2025-05-07 19:22:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>Tajamar Media</title>
 </head>
 <body>
-<h1 align="center">Buenos días, estudiantes </h1>
+<h1 align="center">Buenos días, estudiantes master Tajamar</h1>
 <br>
 <div align="center">
 <img src="playa.jpg">
